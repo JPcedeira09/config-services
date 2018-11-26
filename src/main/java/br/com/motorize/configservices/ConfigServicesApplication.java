1678bb5deecc61389acb52d37b745d648ada642f
Joao Paulo <joao.paulo@hvarconsulting.com.br>
2018-11-26 13:16:23 -0200

    Initial commit

---
+++ src/main/java/br/com/motorize/configservices/ConfigServicesApplication.java
@@ -0,0 +1,14 @@
+package br.com.motorize.configservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServicesApplication.class, args);
+	}
+}
